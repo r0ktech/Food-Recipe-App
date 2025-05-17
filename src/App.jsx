b0e03bcd421e3239af2d825a9a0f52cec52a9bca
Raphael <raphaelokeke002@gmail.com>
2025-05-17 14:41:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Search from "./Search";
+import { useState } from "react";
+import FoodList from "./FoodList";
+import Nav from "./Nav";
+import "./App.css";
+import Container from "./Container";
+import InnerContainer from "./InnerContainer";
+import FoodDetails from "./FoodDetails";
+
+function App() {
+  const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("656329");
+  return (
+    <div className="App">
+      <Nav />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList setFoodId={setFoodId} foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} />
+        </InnerContainer>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Search.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import styles from "./search.module.css";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "4b7d8939c7e44a3e86a4ce3242ca4c81";
+
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza");
+
+  //Syntax of the useEffect hook
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [query]);
+
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.input}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      ></input>
+    </div>
+  );
+}
