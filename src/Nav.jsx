3cf2a8b0475bbe59c3f5f59ab0d006bfa4880f1e
Raphael Okeke <raphaelokeke002@gmail.com>
2025-05-18 23:08:29 +0100

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,5 +1,5 @@
 import styles from "./nav.module.css";
 
 export default function Nav() {
-  return <div className={styles.nav}> 🍝 FoodApp</div>;
+  return <div className={styles.nav}> 🍝FoodApp</div>;
 }
